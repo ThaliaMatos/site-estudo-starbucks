8261c3b495f5c012b8783f0b18df38fa90a1d350
Thalia Matos <thalia.matos3197@gmail.com>
2024-03-19 16:05:05 -0300

    addCirculijs

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-let quadrado = document.querySelector('.quadrado')
-    let imgCopo = document.querySelector('.starbuks')
-    let verde = document.querySelector('.verde')
-    let amarelo = document.querySelector('.amarelo')
-    let rosa = document.querySelector('.rosa')
+let circulo = document.querySelector('.circulo')
+let imgCopo = document.querySelector('.starbucks')
+let verde = document.querySelector('.verde')
+let amarelo = document.querySelector('.amarelo')
+let rosa = document.querySelector('.rosa')
 
 
-    verde.addEventListener('click', () => {
-        imgCopo.src = "./img/img1.png"
-        quadrado.style.background = "#01a661"
-    })
-    
-    amarelo.addEventListener('click', () => {
-        imgCopo.src = "./img/img2.png"
-        imgCopo.style.width = '340px'
-        quadrado.style.background = "#EB7495"
-    })
-    
-    rosa.addEventListener('click', () => {
-        imgCopo.src = "./img/img3.png"
-        quadrado.style.background = "#d752b1"
-    })
+verde.addEventListener('click', () => {
+    imgCopo.src = "./img/img1.png"
+    circulo.style.background = "#01a661"
+    imgCopo.style.width = '';
+})
+
+amarelo.addEventListener('click', () => {
+    imgCopo.src = "./img/img2.png"
+    circulo.style.background = "#EB7495";
+    imgCopo.style.width = '';
+})
+
+rosa.addEventListener('click', () => {
+    imgCopo.src = "./img/img3.png"
+    circulo.style.background = "#d752b1";
+    imgCopo.style.width = '';
+})
